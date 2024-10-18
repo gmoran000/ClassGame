abaa5187f60540be19263c74132b8ef5974a1c1b
gmoran000 <gmoran000@citymail.cuny.edu>
2024-10-17 22:16:49 -0400

    platformer draft

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private Transform player;
+    [SerializeField] private float aheadDistance;
+    [SerializeField] private float cameraSpeed;
+    private float lookAhead;
+
+    private void Update()
+    {
+        //Room camera
+        //transform.position = Vector3.SmoothDamp(transform.position, new Vector3(currentPosX, transform.position.y, transform.position.z), ref velocity, speed);
+
+        //Follow player
+        transform.position = new Vector3(player.position.x + lookAhead, player.position.y, transform.position.z);
+        lookAhead = Mathf.Lerp(lookAhead, (aheadDistance * player.localScale.x), Time.deltaTime * cameraSpeed);
+    }
+}
